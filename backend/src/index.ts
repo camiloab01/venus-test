d36b03aa6d6c17a91c783c20e587cd87adda12b2
Camilo A <camiloab01@gmail.com>
2025-05-06 12:12:47 -0400

    Move prettier to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
-import express, { Express, Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import { db } from './config/db';
-import marketRouter from './modules/market/market.route';
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { db } from "./config/db";
+import marketRouter from "./modules/market/market.route";
 
 dotenv.config();
 
@@ -17,18 +17,18 @@ const testDbConnection = async () => {
   try {
     const connection = await db().getConnection();
     connection.release();
-    return 'Database connection established successfully';
+    return "Database connection established successfully";
   } catch (error) {
     return `Error connecting to database: ${error}`;
   }
 };
 
-app.get('/', async (req: Request, res: Response) => {
+app.get("/", async (req: Request, res: Response) => {
   const response = await testDbConnection();
   res.json({ response });
 });
 
-app.use('/api/v1/markets', marketRouter);
+app.use("/api/v1/markets", marketRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);

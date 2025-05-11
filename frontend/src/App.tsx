3e15462d4e7343cbee1c6219c11a291c5f1a76d3
Camilo A <camiloab01@gmail.com>
2025-05-10 21:22:30 -0600

    Design for mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,10 @@ import './App.css'
 
 function App() {
   return (
-    <div className="h-[265px] w-[976px] bg-[#282931] rounded-[24px] bg-[url('/src/assets/illustration.png')] bg-[length:715px_624px] bg-no-repeat bg-cover bg-right-top">
-      <div className="flex flex-col items-start h-full p-8 gap-4">
-        <div className="flex flex-col gap-2 items-start">
+    <div className="h-[490px] w-[359px] bg-white/[4%] rounded-[24px]">
+      <div className="h-1/2 bg-[url('/src/assets/illustration.png')] bg-[length:410px_358px] bg-no-repeat bg-cover bg-right-top"></div>
+      <div className="h-1/2 flex flex-col items-center p-8 gap-4 bg-[#282931] rounded-b-[24px]">
+        <div className="flex flex-col gap-2 items-center">
           <p className="text-lg leading-[27px] text-white font-semibold">
             Treasury balance
           </p>
@@ -12,7 +13,7 @@ function App() {
             1,111.111111111111111111 XVS
           </p>
         </div>
-        <div className="flex flex-col gap-2 items-start">
+        <div className="flex flex-col gap-2 items-center">
           <p className="text-lg leading-[27px] text-white font-semibold">
             Market size
           </p>
@@ -22,7 +23,7 @@ function App() {
         </div>
         <button
           type="button"
-          className="text-white bg-[#3A78FF] hover:bg-blue-600 font-semibold rounded-lg text-md px-5 py-2.5 focus:outline-none"
+          className="text-white bg-[#3A78FF] hover:bg-blue-600 font-semibold rounded-lg text-md px-5 py-2.5 focus:outline-none w-full"
         >
           Refresh
         </button>

1ec17b877bf51324c072140493ec53377bd83262
Camilo A <camiloab01@gmail.com>
2025-05-12 14:59:13 -0600

    Add cors to BE

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,15 @@
 import express from "express";
 import marketRouter from "./modules/market/market.route";
+import cors from "cors";
 
 export const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5174",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    credentials: true,
+  }),
+);
+
 app.use("/api/v1/markets", marketRouter);

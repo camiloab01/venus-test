b554c99522d4809e262852b5624e9bd908867fd8
Camilo A <camiloab01@gmail.com>
2025-05-12 11:02:09 -0600

    More responsiveness WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,9 +2,9 @@ import './App.css'
 
 function App() {
   return (
-    <div className="h-[490px] md:h-[254px] w-[359px] md:w-[792px] md:bg-[#282931] md:bg-[url('/src/assets/illustration.png')] md:bg-[length:715px_624px] md:bg-no-repeat md:bg-cover md:bg-right-top bg-white/[4%] rounded-[24px]">
+    <div className="h-[490px] md:h-[254px] lg:h-[265px] w-[359px] md:w-[792px] lg:w-[976px] md:bg-[#282931] bg-white/[4%] rounded-[24px]">
       <div className="block md:hidden h-1/2 bg-[url('/src/assets/illustration.png')] bg-[length:410px_358px] bg-no-repeat bg-cover bg-right-top"></div>
-      <div className="h-1/2 flex flex-col md:items-start items-center p-8 gap-4 bg-[#282931] rounded-b-[24px]">
+      <div className="h-1/2 md:h-full flex flex-col md:items-start items-center p-8 gap-4 md:bg-[url('/src/assets/illustration.png')] md:bg-[length:715px_624px] md:bg-no-repeat md:bg-cover md:bg-right-top bg-white/[4%] rounded-b-[24px] md:rounded-[24px]">
         <div className="flex flex-col gap-2 md:items-start items-center">
           <p className="text-lg leading-[27px] text-white font-semibold">
             Treasury balance
